ccbcb0d9d0071052934a95eacba3f0b5c37517bd
mateo vukusic <mateo.vukusic@krokodil.hr>
2019-04-12 13:39:40 +0200

    add logic for encrypt xlsx

--- lib/protectify/xlsx_protectify.rb
+++ lib/protectify/xlsx_protectify.rb
@@ -1,8 +1,54 @@
+require 'java_initializer.rb'
+
 module Protectify
     class XlsxProtectify
         def initialize(path, password)
             @path = path
             @password = password
         end
+
+        def call
+            initialize_java_class
+            encryptor_password
+            write_into_encrypted_output_stream
+            file_output_stream
+        end
+
+        private
+
+        attr_reader :path, :password
+
+        def initialize_java_class
+            @fs = POI_FILE_SYSTEM.new
+            @encryption_mode = ENCRYPTION_MODE
+            @info = ENCRYPTION_INFO.new(@encryption_mode.agile)
+            @fj = FILE_JAVA.new(path)
+            @is = FILE_INPUT_STREAM.new(@fj)
+          end
+
+          def encryptor_password
+            @enc = @info.getEncryptor
+            @enc.confirmPassword(password)
+          end
+
+          def write_into_encrypted_output_stream
+            @os = @enc.getDataStream(@fs)
+            IO_UTILS.copy(@is, @os)
+            @os.close
+            @is.close
+          end
+
+          def file_output_stream
+            delete_file
+            @path = FILE_JAVA.new(path)
+            @fos = FILE_OUTPUT_STREAM.new(@path)
+            @fs.writeFilesystem(@fos)
+            @fos.close
+            @fs.close
+          end
+
+          def delete_file
+            File.delete(path)
+          end
     end
 end
